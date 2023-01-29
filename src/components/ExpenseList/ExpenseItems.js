c04bc3cca6ff9280edb4e79675f184f34ffb91a7
tej00001 <tejachintu207@gmail.com>
2023-01-29 15:37:31 +0530

    Adding task3

--- src/components/ExpenseList/ExpenseItems.js
+++ src/components/ExpenseList/ExpenseItems.js
@@ -2,35 +2,56 @@ import "./ExpenseItems.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
+import React,{useState} from "react";
+
 
 function ExpenseItems(props) {
+  
+const [amount, setAmount] = useState(props.amount);
+
+const changeHandler=()=>{
+  setAmount('$100');
+  console.log(amount)
+}
+const [name, setName] = useState(props.name);
+
+const expenseHandler=()=>{
+  setName('updated!');
+  console.log(name)
+
+}
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.name}</h2>
+        <h2>{name}</h2>
+      <button onClick={changeHandler}>Add Expenses</button>
+
         <ExpenseDetails
-          amount={props.amount}
+          amount={amount}
           LocationOfExpenditure={props.LocationOfExpenditure}
         ></ExpenseDetails>
       </div>
+      <button onClick={expenseHandler}>Add Expenses</button>
+
     </Card>
   );
 }
 
-// function ExpenseItems(props) {
-//   return (
-//     <div className="expense-item">
-//       <ExpenseDate date={props.data.date}></ExpenseDate>
-//       <ExpenseDetails
-//         amount={props.data.amount}
-//         LocationOfExpenditure={props.data.LocationOfExpenditure}
-//       ></ExpenseDetails>
-//       <div className="expense-item__description">
-//         <h2>{props.data.name}</h2>
-//       </div>
-//     </div>
-//   );
-// }
+//  function ExpenseItems(props) {
+// return (
+// <div className="expense-item">
+//        <ExpenseDate date={props.data.date}></ExpenseDate>
+//        <div className="expense-item__description">
+//          <h2>{props.data.name}</h2>
+//        <ExpenseDetails
+//            amount={props.data.amount}
+//          LocationOfExpenditure={props.data.LocationOfExpenditure}
+//        ></ExpenseDetails>
+
+//      </div>
+//      </div>
+//    );
+//  }
 
 export default ExpenseItems;
